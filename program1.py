47190a81a706e0a98c14212eda43119ec556b103
Gerald Tankersley <tank7575@yahoo.com>
2019-04-27 10:11:23 -0500

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,45 @@
+import os
+import webbrowser
+# os.startfile("Chrome")
+# os.startfile("firefox")
+# os.startfile("Outlook")
+
+
+webbrowser.open("www.google.com")
+
+
+
+
+
+
+
+
+
+# import subprocess
+# subprocess.call(['notepad.exe', 'abc.txt'], shell = true)
+#  subprocess.Popen(['notepad.exe'])
+# # thread continues ...
+# #p.terminate()
+#p = subprocess.Popen('')
+
+
+# import os
+# os.startfile('notepade.exe')
+
+
+# import os
+# os.system("notebook.exe")
+
+
+#
+# with open('readme.txt', 'r') as f:
+#     pass
+
+
+
+# f = open('readme.txt', 'r')
+# print(f.name)
+# f.close()
+
+
+
